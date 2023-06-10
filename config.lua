43d279953245c2fc85136206d06160ba52f31812
Juergen Belada <juergen.belada@annny.com>
2023-06-10 12:16:03 +0200

    first commit

---
+++ config.lua
@@ -0,0 +1,54 @@
+local _M = {}
+
+_M.BizhawkDir = "C:/Users/jobol/Downloads/BizHawk-2.2.2/"
+
+_M.StateDir = _M.BizhawkDir .. "Lua/SNES/neat-mario/state/"
+_M.PoolDir = _M.BizhawkDir .. "Lua/SNES/neat-mario/pool/"
+
+
+_M.State = {
+ 			"DP1.state",				
+			"YI1.state",				
+			"YI2.state",				
+}
+
+
+_M.StartPowerup = 0
+
+_M.NeatConfig = {
+Filename = _M.PoolDir .. _M.State[1],
+Population = 300,
+DeltaDisjoint = 2.0,
+DeltaWeights = 0.4,
+DeltaThreshold = 1.0,
+StaleSpecies = 15,
+MutateConnectionsChance = 0.25,
+PerturbChance = 0.90,
+CrossoverChance = 0.75,
+LinkMutationChance = 2.0,
+NodeMutationChance = 0.50,
+BiasMutationChance = 0.40,
+StepSize = 0.1,
+DisableMutationChance = 0.4,
+EnableMutationChance = 0.2,
+TimeoutConstant = 20,
+MaxNodes = 10000000,
+}
+
+_M.ButtonNames = {
+		"A",
+		"B",
+		"X",
+		"Y",
+		"Up",
+		"Down",
+		"Left",
+		"Right",
+	}
+	
+_M.BoxRadius = 8
+_M.InputSize = (_M.BoxRadius*2+1)*(_M.BoxRadius*2+1)
+
+_M.Running = false
+
+return _M
